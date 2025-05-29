aa9d68eb6e6218f858fafabe20184c99d0b6a3aa
Android Studio <devandroid@ort>
2025-05-29 11:46:47 -0300

    home y personas terminados (funcionalidad, agregar estilos pendiente)

--- TpDirectorio/src/components/Home.jsx
+++ TpDirectorio/src/components/Home.jsx
@@ -1,9 +1,26 @@
+import personas from "../assets/people.json";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { NavLink } from "react-router-dom";
+import "./home.css"
 
 function Home() {
   return (
-    <div>
-      <h1>Mi Aplicación</h1>
-    </div>
+    <ul className="parent" style={{ listStyleType: "none" }}>
+      {personas.map((persona) => (
+        <li className="" key={persona.id}>
+          <Card style={{ width: '18rem', marginBottom: '1rem' }}>
+            <Card.Body>
+              <Card.Title>{persona.nombre} {persona.apellido}</Card.Title>
+              <Card.Text>{persona.edad} años</Card.Text>
+              <Button variant="primary" as={NavLink} to={`/persona/${persona.id}`}>
+                Ver más
+              </Button>
+            </Card.Body>
+          </Card>
+        </li>
+      ))}
+    </ul>
   );
 }
 
--- TpDirectorio/src/components/PersonaCard.jsx
+++ TpDirectorio/src/components/PersonaCard.jsx
@@ -1,11 +1,34 @@
+import Card from 'react-bootstrap/Card';
+import { useParams } from 'react-router-dom';
+import personas from "../assets/people.json";
+
+
 
 function PersonaCard() {
-    return (
-      <div>
-        <h1>Mi Aplicación</h1>
-      </div>
+  const { id } = useParams();
+  const persona = personas.find(p => p.id === parseInt(id));
+
+  if (!persona) return <p>Persona no encontrada</p>;
+  else
+  {
+  let condicionEdad;
+  if (persona.edad >= 18) { condicionEdad = "Mayor de edad" } else { condicionEdad = "Menor de edad" }
+  return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{persona.nombre}</Card.Title>
+          <Card.Text>
+            <ul>
+            <li>Nombre Completo: {persona.nombre} {persona.apellido}</li>
+            <li>Email: {persona.email}</li>
+            <li>Edad: {persona.edad} años ({condicionEdad})</li>
+            
+            </ul>
+          </Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
-  
-  export default PersonaCard;
-  
+}
+
+export default PersonaCard;
